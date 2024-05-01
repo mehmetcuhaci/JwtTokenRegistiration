dfc54bc91a54864e99adf32a4bd35745741b9056
Mehmet Ali Çuhacı <mehmet.cuhaci10@gmail.com>
2024-05-01 16:41:09 +0300

    User takes new token when sign in

--- JwtSignFinal/Controllers/SignInController.cs
+++ JwtSignFinal/Controllers/SignInController.cs
@@ -1,7 +1,10 @@
-﻿using JwtSignFinal.Models;
+﻿using JwtSignFinal.Handler;
+using JwtSignFinal.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace JwtSignFinal.Controllers
 {
@@ -11,11 +14,48 @@ namespace JwtSignFinal.Controllers
     
     public class SignInController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult SignIn()
+        private readonly DataContext _dataContext;
+        private readonly IConfiguration _configuration;
+        public SignInController(DataContext dataContext, IConfiguration configuration)
         {
+            _dataContext = dataContext;
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public async Task< IActionResult> SignIn(string userName,string passWord)
+        {
+
+            if (string.IsNullOrWhiteSpace(userName)||string.IsNullOrWhiteSpace(passWord))
+            {
+                return BadRequest("Kullanıcı adı veya şifre boş olamaz!");
+            }
+
+            var user=await _dataContext.Users
+                .FirstOrDefaultAsync(u=>u.UserName == userName);
             
-            return Ok("Admin girdi");
+            if (user == null)
+            {
+                return Unauthorized("Kullanıcı bulunamadı");
+            }
+
+            if(user.Password!=passWord)
+            {
+                return Unauthorized("Şifre yanlış");
+            }
+
+            bool isEmailVerified = user.IsEmailVerified == 1;
+
+            if (!isEmailVerified)
+            {
+                return Unauthorized("Email has not been verified.");
+            }
+            user.Token = null;
+            Token token = TokenHandler.CreateToken(_configuration, user);
+            user.Token = token.AccessToken;
+            await _dataContext.SaveChangesAsync();
+
+            return Ok("Admin Girdi");
+
         }
 
 
